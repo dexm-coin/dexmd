ca2ddb6c3866b2a92a8de4f8f9d6fc4d6c3d57a6
0x5eba <sbiollo@gmail.com>
2018-04-30 16:01:47 +0200

    Modified createVote and add isVoteValid

--- blockchain/casper.go
+++ blockchain/casper.go
@@ -5,31 +5,32 @@ import (
 	"fmt"
 
 	"github.com/dexm-coin/dexmd/wallet"
-	"github.com/dexm-coin/protobufs/build/network"
+	"github.com/dexm-coin/protobufs/build/blockchain"
+	protobufs "github.com/dexm-coin/protobufs/build/blockchain"
+	"github.com/golang/protobuf/proto"
+	log "github.com/sirupsen/logrus"
 )
 
-// Vote is the struct of a vote in casper
-type Vote struct {
-	s         string            // hash of the source checkpoint
-	t         string            // hash of the descendent of s
-	hS        uint64            // height of s
-	hT        uint64            // height of t
-	signature network.Signature // signature of <s, t, h(s), h(t)> with validator private key
-}
-
 // Create a vote based on casper Vote struct
-func createVote(sVote, tVote string, hsVote, htVote uint64, w *wallet.Wallet) Vote {
+// CasperVote:
+// - Source -> hash of the source checkpoint
+// - Target -> hash of any descendent of s
+// - SourceHeight -> height of s
+// - TargetHeight -> height of t
+// - R, S -> signature of <s, t, h(s), h(t)> with validator private key
+func createVote(sVote, tVote string, hsVote, htVote uint64, w *wallet.Wallet) blockchain.CasperVote {
 	data := []byte(sVote + tVote + fmt.Sprintf("%v", hsVote) + fmt.Sprintf("%v", hsVote))
 	bhash := sha256.Sum256(data)
 	hash := bhash[:]
-	r, s, _ := w.Sign(hash)
-	pub, _ := w.GetPubKey()
-	return Vote{sVote, tVote, hsVote, htVote, network.Signature{
-		Pubkey: pub,
-		R:      r.Bytes(),
-		S:      s.Bytes(),
-		Data:   hash,
-	}}
+	rSign, sSign, _ := w.Sign(hash)
+	return blockchain.CasperVote{
+		Source:       []byte(sVote),
+		Target:       []byte(tVote),
+		SourceHeight: hsVote,
+		TargetHeight: htVote,
+		R:            rSign.Bytes(),
+		S:            sSign.Bytes(),
+	}
 }
 
 // Every checkpoint there should be an agreement of
@@ -40,8 +41,7 @@ func checkpointAgreement() {
 
 // A checkpoint c is called justified if it is the root,
 // or if there exists a supermajority link c to any of its
-// direct children c' in the checkpoint tree, where c' is
-// justified
+// direct children c' in the checkpoint tree, where c' is justified
 func isJustified() {
 
 }
@@ -51,4 +51,24 @@ func isJustified() {
 // direct children in the checkpoint tree
 func isFinalized() {
 
+}
+
+// IsVoteValid check if s is an ancestor of t in the chain
+func IsVoteValid(b *Blockchain, source *protobufs.Block, target *protobufs.Block) bool {
+	if len(b.Validators.valsArray) < 1 {
+		log.Error("No validators")
+		return false
+	}
+	for i := target.Index - 1; i > 0; i-- {
+		byteBlock, _ := b.blockDb.Get([]byte(string(i)), nil)
+		blocks := &blockchain.Index{}
+		proto.Unmarshal(byteBlock, blocks)
+
+		for _, block := range blocks.GetBlocks() {
+			if block == source {
+				return true
+			}
+		}
+	}
+	return false
 }
