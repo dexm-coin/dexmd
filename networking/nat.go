3886075af72501ecee7329e571c670e59d671487
BadLamb <lambda@Antonios-MBP.fritz.box>
2018-08-23 22:02:32 +0200

    Add NAT traversal

---
+++ networking/nat.go
@@ -0,0 +1,60 @@
+package networking
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/NebulousLabs/go-upnp"
+)
+
+// InitializeNatTraversal opens the port passed as an arguemnt and returns
+// ip:port in a string. Required manaually closing port
+func InitializeNatTraversal(port uint16, desc string) (string, error) {
+	// Connect to router
+	d, err := upnp.Discover()
+	if err != nil {
+		return "", err
+	}
+
+	ip, err := d.ExternalIP()
+	if err != nil {
+		return "", err
+	}
+
+	// Remove forwarding map and ignore error in case it doesn't exist
+	d.Clear(port)
+
+	// Open the port
+	err = d.Forward(port, desc)
+
+	return fmt.Sprintf("%s:%d", ip, port), nil
+}
+
+// FindPeers tries to find all peers for the selected network
+func (cs *ConnectionStore) FindPeers() error {
+	peerURL := fmt.Sprintf("https://%s.dexm.space/peers", cs.network)
+
+	resp, err := http.Get(peerURL)
+	if err != nil {
+		return err
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	ips := []string{}
+	err = json.Unmarshal(data, &ips)
+	if err != nil {
+		return err
+	}
+
+	for _, i := range ips {
+		cs.Connect(i)
+	}
+
+	return nil
+}
