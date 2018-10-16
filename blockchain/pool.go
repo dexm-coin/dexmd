11937b96e10decb10ef6c2f3e8adb612ee66d42f
BadLamb <antoniogroza@gmail.com>
2018-10-16 14:48:36 +0200

    fml

--- blockchain/pool.go
+++ blockchain/pool.go
@@ -119,7 +119,7 @@ func (bc *Blockchain) GenerateBlock(miner string, shard uint32, validators *Vali
 		}
 
 		// remove transaction from db as it was included
-		_ = bc.BlockDb.Delete(txKey, nil)
+		_ = bc.BlockDb.Delete(*txKey, nil)
 
 		// Don't include invalid transactions
 		err = bc.ValidateTransaction(rtx)
